952ab1ab00ff69c635d6fa66b417c249d4eb61da
icastellsg <icastellsg01@gmail.com>
2024-07-09 20:14:06 +0200

    feat: basic prediction on web

---
+++ web/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import numpy as np
+
+app = Flask(__name__)
+
+model = load_model('../LSMTTensorflow/bestModel_60.keras')
+
+@app.route('/')
+def predict():
+    return render_template('predict.html')
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello_name(name = None):
+    return render_template('hello_there.html', name=name)
+
+@app.route('/predict/', methods=['POST'])
+def predict_temperature_suntracer():
+    input_data = float(request.form['temperatura'])
+    
+    prediction = model.predict(np.array([[input_data]]))
+    
+    prediction = prediction[0][-1]
+        
+    return render_template('predict.html', prediction=prediction)

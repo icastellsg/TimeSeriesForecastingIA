c823aec555b9ee12d32ff45b0c9bf88dbd91d923
icastellsg <icastellsg01@gmail.com>
2024-07-10 18:39:17 +0200

    feat: print graphs from predictions

--- web/app.py
+++ web/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
 from forms import SuntracerForm
 import tensorflow as tf
+from io import BytesIO
+import base64
+from matplotlib.figure import Figure
 from tensorflow.keras.models import load_model
 import numpy as np
 
@@ -10,7 +13,6 @@ app.config['SECRET_KEY'] = 'secret'
 model1h = load_model('../LSMTTensorflow/bestModel_60.keras')
 model6h = load_model('../LSMTTensorflow/bestModel_360.keras')
 model12h = load_model('../LSMTTensorflow/bestModel_720.keras')
-
 
 @app.route('/')
 @app.route('/predict/')
@@ -24,14 +26,26 @@ def predict_temperature_suntracer():
         prediction = []
         temperatura = np.array(form.temperatura.data)
         
-        prediction.append(get_prediction(model1h, temperatura))
-        prediction.append(get_prediction(model6h, temperatura))
-        prediction.append(get_prediction(model12h, temperatura))
+        prediction.append(get_prediction(model1h, temperatura)[0][-1])
+        prediction.append(get_prediction(model6h, temperatura)[0][-1])
+        prediction_12h = get_prediction(model12h,temperatura)
         
+        prediction.append(prediction_12h[0][-1])
         
-        return render_template('predict_with_flaskForms.html', form=form, prediction=prediction)
+        #Save latest plot
+        fig = Figure()
+        
+        ax = fig.subplots()
+        ax.plot(prediction_12h.flatten())
+        
+        buf = BytesIO()
+        fig.savefig(buf, format="png")
+        
+        data = base64.b64encode(buf.getbuffer()).decode("ascii")
+        
+        return render_template('predict_with_flaskForms.html', form=form, prediction=prediction, img_data=data)
     return render_template('predict_with_flaskForms.html', form=form)
 
 def get_prediction(model, value):
     predict = model.predict(np.array([[value]]))
-    return predict[0][-1]
+    return predict
